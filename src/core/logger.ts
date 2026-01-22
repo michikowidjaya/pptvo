b3eb2459d25ead705f557c81d702792b3bd7cac3
Hana Azizah Nurhadi <hanazizahn@gmail.com>
2026-01-22 13:18:52 +0700

    Delete src directory

--- src/core/logger.ts
+++
@@ -1,5 +0,0 @@
-export const log = {
-  info: (...args: unknown[]) => console.log("[info]", ...args),
-  warn: (...args: unknown[]) => console.warn("[warn]", ...args),
-  error: (...args: unknown[]) => console.error("[error]", ...args)
-};
